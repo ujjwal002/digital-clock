ccd278c6e2db93ea910f97a81c2e480c35aaa3b8
ujjwal kumar <79785180+ujjwal002@users.noreply.github.com>
2022-11-09 02:49:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// for going in different section control shift e;
+const hourEl =document.getElementById("hour");
+const minuteEl=document.getElementById("Minute");
+const secondSl =document.getElementById("Second")
+const ampmEl= document.getElementById("ampm");
+
+function updateClock(){
+  let h  =new Date().getHours();
+  let m= new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm ="AM";
+  if(h>12){
+    h =h-12;
+    ampm="PM";
+  }
+
+  h=h<10 ? "0"+h:h;
+  m=m<10 ? "0"+m:m;
+  s=s<10 ? "0"+s:s;
+
+
+
+  hourEl.innerText =h ;
+  minuteEl.innerText = m;
+  secondSl.innerText = s;
+  ampmEl,(innerText = ampm);
+  setTimeout(()=>{
+    updateClock()
+   
+  },1000)
+}
+updateClock()
